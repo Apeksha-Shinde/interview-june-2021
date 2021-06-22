2b54b0cc621d15b1d25b49a87032a456ddacb2c5
Apeksha-Shinde <86234461+Apeksha-Shinde@users.noreply.github.com>
2021-06-22 12:23:05 +0530

    Update ConvertToBinary.java
    import java.util.*;
    package exam;
    
    public class ConvertToBinary {
    
      public String toBinary(final int n) {
        //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
        Stack<Integer> stack = new Stack<Integer>();
     
       
     
        while (n != 0)
        {
          int d = n % 2;
          stack.push(d);
          n /= 2;
        } 
     string s;
        while (!(stack.isEmpty() ))
        {
          s=s+stack.pop();
        }
    return s;
      
      }
    }

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,26 @@
+import java.util.*;
 package exam;
 
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    Stack<Integer> stack = new Stack<Integer>();
+ 
+   
+ 
+    while (n != 0)
+    {
+      int d = n % 2;
+      stack.push(d);
+      n /= 2;
+    } 
+ string s;
+    while (!(stack.isEmpty() ))
+    {
+      s=s+stack.pop();
+    }
+return s;
+  
   }
 }
